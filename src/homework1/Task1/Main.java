dd68db53cba5fe8b66b81430d0dfdb14215be500
Serega S <serega1067@yandex.ru>
2023-10-03 01:10:33 +0700

    Added a task condition

--- src/homework1/Task1/Main.java
+++ src/homework1/Task1/Main.java
@@ -2,6 +2,13 @@ package homework1.Task1;
 
 import static org.assertj.core.api.Assertions.*;
 
+// Задание 1. ** В классе Calculator создайте метод calculateDiscount,
+// который принимает сумму покупки и процент скидки и возвращает сумму
+// с учетом скидки. Ваша задача - проверить этот метод с использованием
+// библиотеки AssertJ. Если метод calculateDiscount получает недопустимые
+// аргументы, он должен выбрасывать исключение ArithmeticException.
+// Не забудьте написать тесты для проверки этого поведения.
+
 public class Main {
     public static void main(String[] args) {
 
